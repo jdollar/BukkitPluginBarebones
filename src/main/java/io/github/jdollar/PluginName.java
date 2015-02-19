b75459387cce2473694edd145f91da3103b77ec1
Joel Dollarhide <jdollarhide@systemsinmotion.com>
2015-02-19 15:39:02 -0500

    Initial commit for the skeleton project

---
+++ src/main/java/io/github/jdollar/PluginName.java
@@ -0,0 +1,10 @@
+package io.github.jdollar;
+
+/**
+ * Created by jdollarhide on 2/19/2015.
+ */
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PluginName extends JavaPlugin {
+}
